c5b5e72a6c0ad69db96ba5082ade8d3e4563a5ec
wsruser1 <wsrwser1@mail.ru>
2021-09-07 14:35:03 +0300

    asd

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by user on 07.09.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var opertor = ""
+    var numberOne = ""
+    var numberTwo = ""
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var UIButton: UIButton!
+    
+    @IBAction func inputNumber(_ sender: UIButton) {//функция для обработки чисел от 0 до 9
+        if opertor.isEmpty {
+            numberOne = numberOne + ( sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        }else{
+            numberTwo = numberTwo + ( sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
     }
-
-
+    
+    @IBAction func clearAction(_ sender: UIButton) {//функция для очистки
+        opertor = ""
+        numberTwo = ""
+        numberOne = ""
+        resultLabel.text = "0"
+    }
+    
+    @IBAction func inputOpertor(_ sender: UIButton) {
+        opertor = sender.titleLabel?.text as! String
+    }
+    
+    @IBAction func resultAction(_ sender: Any) {
+        var result = 0.0
+        switch opertor {
+        case "/":
+            result = Double(numberOne)!/Double(numberTwo)!
+        case "*":
+            result = Double(numberOne)! * Double(numberTwo)!
+        case "+":
+            result = Double(numberOne)! + Double(numberTwo)!
+        case "-":
+            result = Double(numberOne)! - Double(numberTwo)!
+        default:
+            break
+        }
+        if result.truncatingRemainder(dividingBy: 1.0) == 00 {
+            resultLabel.text = String(Int(result))
+        } else {
+            resultLabel.text = String(result)
+        }
+    }
 }
 
+ 
